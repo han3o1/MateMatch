a5d1b061dce2253a45ec6857f4fb94fbf46789d9
han3o1 <skqieum@gmail.com>
2025-11-01 20:39:34 +0900

    feat: Implement Firebase email/password signup and login

--- app/src/main/java/com/mp/matematch/MateMatchApplication.kt
+++ app/src/main/java/com/mp/matematch/MateMatchApplication.kt
@@ -1,4 +1,13 @@
 package com.mp.matematch
 
-class MateMatchApplication {
+import android.app.Application
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
+
+class MateMatchApplication : Application() { // 👈 이 부분이 중요합니다.
+    override fun onCreate() {
+        super.onCreate()
+        // 앱이 시작될 때 Firebase를 초기화합니다.
+        Firebase.initialize(this)
+    }
 }
